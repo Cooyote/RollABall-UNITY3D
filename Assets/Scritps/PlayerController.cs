df0e83696b5a9af59ca4466d4ee5207d2c8b358b
Coyote <mike.w.coyote@gmail.com>
2016-09-24 22:41:06 -0300

    Collection,scoring and building the game

--- Assets/Scritps/PlayerController.cs
+++ Assets/Scritps/PlayerController.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+
 
 public class PlayerController : MonoBehaviour 
 {
-	private Rigidbody rb;
+	
 	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
 
 	// Use this for initialization
 	void Start () 
 	{
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate ()
@@ -20,10 +30,23 @@ public class PlayerController : MonoBehaviour
 		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
 		rb.AddForce (movement * speed);
 	}
-		
-	// Update is called once per frame
-	void Update () 
+
+	void OnTriggerEnter(Collider other)
 	{
-	
+		if (other.gameObject.CompareTag ("PickUp")) 
+		{
+			other.gameObject.SetActive (false);	
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) 
+		{
+			winText.text = "You Win!";	
+		}
 	}
 }
